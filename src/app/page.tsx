2f9a5583b88eb9bb8a85ca27dce95b7dddd98fe6
rahul palamarthi <rahulpalamarthi@gmail.com>
2023-10-21 13:02:57 +0530

    feat: add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@ export default function Home() {
 	return (
 		<main>
 			<section>
-				<div className='max-w-7xl mx-auto'>
-					<h2>Home</h2>
-					<p>Public Route</p>
+				<div className='max-w-7xl mx-auto py-5'>
+					<h2 className='text-3xl font-semibold'>Home</h2>
+					<p>
+						A <strong>Next.js</strong> app with <strong>Typescript</strong> that implements role based auth using <strong>Next-auth</strong> with
+						mongoDB adapter
+					</p>
 				</div>
 			</section>
 		</main>
